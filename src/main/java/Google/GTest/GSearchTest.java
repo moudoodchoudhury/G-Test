3c2b413efe71f113436f816f65120558738b81b8
Moudood Choudhury <moudood_chy@yahoo.com>
2019-01-27 20:26:38 -0500

    this is my G-test automation project

---
+++ src/main/java/Google/GTest/GSearchTest.java
@@ -0,0 +1,17 @@
+package Google.GTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GSearchTest {
+
+	public static void main(String[] args) {
+		
+		System.setProperty(key, value)
+		WebDriver driver = new ChromeDriver();
+		driver.navigate(ww)
+		
+		
+	}
+
+}
